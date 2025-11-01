e3f00205078dbcb7a7a2966c73fcd65f195abfbc
Rebeca D.Montenegro <rebediazmontenegro@gmail.com>
2025-11-01 13:04:41 +0100

    Terminado

--- scripts.js
+++ scripts.js
@@ -32,35 +32,59 @@ const militaryHelicopter = [
 
 class Gallery {
     constructor(civilImages, militaryImages) {
+        this.civilImages = civilImages;
+        this.militaryImages = militaryImages;
     }
     
     getRandomCivil() {
+        const randomPhoto = Math.floor(Math.random() * this.civilImages.length);
+        return this.civilImages[randomPhoto];
     }
     
     getRandomMilitary() {
+        const randomPhoto = Math.floor(Math.random() * this.militaryImages.length);
+        return this.militaryImages[randomPhoto];
     }
     
     getAll() {
+        return [...this.civilImages, ...this.militaryImages];
     }
 }
 
 class Painter {
-    constructor() {
+    constructor(gallery) {
+        this.gallery = gallery;
+        this.createGallery();
     }
 
     createGallery() {
+        this.gallerySection = document.createElement("section");
+        document.body.appendChild(this.gallerySection);
     }
 
     createImageTag(imageUrl) {
+        const picture = document.createElement("picture");
+        const img = document.createElement("img");
+        img.src = imageUrl;
+        img.alt = "Imagen avión";
+        picture.appendChild(img);
+        
+        return picture;
     }
 
     paintSingleImage(imageUrl) {
+        const imagen = this.createImageTag(imageUrl);
+        this.gallerySection.appendChild(imagen);
     }
 
-    paintMultipleImages(arrayOfImages) {
+   paintMultipleImages(arrayOfImages) {
+    arrayOfImages.forEach((imageUrl) => {
+        const picture = this.createImageTag(imageUrl);
+        this.gallerySection.appendChild(picture);
+    });
     }
 }
 
 const aircrafts = new Gallery(civilAircrafts, militaryAircrafts);
 const helicopters = new Gallery(civilHelicopters, militaryHelicopter);
-const painter = new Painter();
+const painter = new Painter(aircrafts);
